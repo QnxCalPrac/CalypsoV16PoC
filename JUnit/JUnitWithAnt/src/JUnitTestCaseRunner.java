14c1782e25ad8dd93048396dab453a6208d4aacd
QnxCalPrac <61448854+QnxCalPrac@users.noreply.github.com>
2020-08-11 14:39:27 +0530

    Update JUnitTestCaseRunner.java
    CALV16-12 : correction to comments

--- JUnit/JUnitWithAnt/src/JUnitTestCaseRunner.java
+++ JUnit/JUnitWithAnt/src/JUnitTestCaseRunner.java
@@ -15,7 +15,7 @@ import com.calypso.tk.service.DSConnection;
 import com.calypso.tk.util.ConnectionUtil;
 
 
-// Test case runner - only
+// Test case runner
 
 public class JUnitTestCaseRunner {
 	public static DSConnection dsCon = null;
