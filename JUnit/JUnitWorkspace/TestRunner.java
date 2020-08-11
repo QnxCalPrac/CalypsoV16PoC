698759e1e9959b29d547591cac3a1d35e3a03693
QnxCalPrac <61448854+QnxCalPrac@users.noreply.github.com>
2020-08-11 12:56:42 +0530

    Update TestRunner.java
    CALV16-12 : added comments for export job test suite

--- JUnit/JUnitWorkspace/TestRunner.java
+++ JUnit/JUnitWorkspace/TestRunner.java
@@ -2,6 +2,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+// Added comments
 public class TestRunner {
 public static void main(String[] args) {
 Result result = JUnitCore.runClasses(TestJUnit.class);
